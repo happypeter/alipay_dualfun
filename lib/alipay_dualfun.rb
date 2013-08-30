c8bd7aae3a512885c4e05ec58596b1a06ed32df6
Peter Wang <happypeter1983@gmail.com>
2013-08-30 11:33:52 +0800

    :key is not 'key'

--- lib/alipay_dualfun.rb
+++ lib/alipay_dualfun.rb
@@ -4,10 +4,10 @@ require 'open-uri'
 require 'digest/md5'
 
 module AlipayDualfun
-  GATEWAY_URL = 'https://mapi.alipay.com/gateway.do'
-  CREATE_PARTNER_TRADE_BY_BUYER_REQUIRED_OPTIONS = %w( service partner _input_charset out_trade_no subject payment_type logistics_type logistics_fee logistics_payment seller_email price quantity )
 
   def self.trade_create_by_buyer_url(options = {})
+    gateway_url = 'https://mapi.alipay.com/gateway.do'
+    trade_create_by_buyer_required_options = %w( service partner _input_charset out_trade_no subject payment_type logistics_type logistics_fee logistics_payment seller_email price quantity )
     options = {
       'service'        => 'trade_create_by_buyer',
       '_input_charset' => 'utf-8',
@@ -17,12 +17,12 @@ module AlipayDualfun
       'logistics_payment' => 'SELLER_PAY'
     }.merge(stringify_keys(options))
 
-    @secret_key = options[:key]
-    options.delete(:key)
+    @secret_key = options['key']
+    options.delete('key')
 
-    check_required_options(options, TRADE_CREATE_BY_BUYER_REQUIRED_OPTIONS)
+    check_required_options(options, trade_create_by_buyer_required_options)
 
-    "#{GATEWAY_URL}?#{query_string(options)}"
+    "#{gateway_url}?#{query_string(options)}"
   end
 
   def self.check_required_options(options, names)
@@ -49,7 +49,6 @@ module AlipayDualfun
      query = params.sort.map do |key, value|
        "#{key}=#{value}"
      end.join('&')
-
      Digest::MD5.hexdigest("#{query}#{@secret_key}")
    end
 end
